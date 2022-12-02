c83915dc76d57bbdbc92523cc43b806fc6efbbf4
Luciano Cavazos <68796551+Luciano-Cavazos@users.noreply.github.com>
2022-12-02 02:29:43 -0500

    Add files via upload

---
+++ Python Syntax Medical Insurance Project.py
@@ -0,0 +1,33 @@
+#init vars
+age=28
+sex=0
+bmi=26.2
+num_of_children=3
+smoker=0
+
+#insurance estimate formula
+insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
+
+print("This person's insurance cost is "+str(insurance_cost)+" dollars.")
+
+#age factor
+age +=4
+new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children
+print("This person's insurance cost is "+str(new_insurance_cost)+" dollars.")
+change_in_insurance_cost=new_insurance_cost-insurance_cost
+print("The change in cost of insurance after increasing the age by 4 years is "+str(change_in_insurance_cost)+" dollars.")
+
+#bmi factor
+age=28
+bmi+=3.1
+new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children
+print("This person's insurance cost is "+str(new_insurance_cost)+" dollars.")
+change_in_insurance_cost=new_insurance_cost-insurance_cost
+print("The change in cost of insurance after increasing the age by 4 years is "+str(change_in_insurance_cost)+" dollars.")
+
+#sex factor
+sex=1
+new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children
+print("This person's insurance cost is "+str(new_insurance_cost)+" dollars.")
+change_in_insurance_cost=new_insurance_cost-insurance_cost
+print("The change in cost of insurance after increasing the age by 4 years is "+str(change_in_insurance_cost)+" dollars.")
